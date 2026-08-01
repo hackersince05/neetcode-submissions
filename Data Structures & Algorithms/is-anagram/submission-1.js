b228ed4ee11c298405b750fc7b63d69e420a9112
EJ <121121522+hackersince05@users.noreply.github.com>
2026-08-01 17:48:04 +0100

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length !== t.length){
+            return false;
+        } 
+        const sortedS = s.split('').sort().join('');
+        const sortedT = t.split('').sort().join('');
+
+        if(sortedS === sortedT) {
+            return true;
+        }
+        return false;
+    }
+}
