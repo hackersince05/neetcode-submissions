7fe7eed1a601944d1a731993322aca0406be988e
EJ <121121522+hackersince05@users.noreply.github.com>
2026-08-02 20:25:15 +0100

    Add: two-integer-sum - submission-21

---
+++ Data Structures & Algorithms/two-integer-sum/submission-21.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const indices = {};
+        for (let i = 0; i < nums.length; i++) {
+            indices[nums[i]] = i; 
+        }
+
+
+        for ( let i = 0; i < nums.length; i++) {
+            
+            let diff = target - nums[i]; 
+            if ( diff in indices && indices[diff] !== i) {
+                return [i, indices[diff]]
+            }
+
+
+        }
+        return [];
+    }
+}
